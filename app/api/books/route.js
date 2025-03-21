55e7513cf8a9dab56c407c700a712ca58495a9fa
Himanshu <himanshu.mohanty@spec-india.com>
2025-03-21 17:34:53 +0530

    changes in

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -3,21 +3,37 @@ import Book from "@/models/book";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { title, author } = await request.json();
-  await connectMongoDB();
-  await Book.create({ title, author });
-  return NextResponse.json({ message: "Book Created" }, { status: 201 });
+  try {
+    const { title, author } = await request.json();
+    await connectMongoDB();
+    await Book.create({ title, author });
+    return NextResponse.json({ message: "Book Created" }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
 
 export async function GET() {
-  await connectMongoDB();
-  const books = await Book.find().limit(50);
-  return NextResponse.json({ books });
+  try {
+    await connectMongoDB();
+    const books = await Book.find().limit(50);
+    return NextResponse.json({ books });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
 
 export async function DELETE(request) {
-  const id = request.nextUrl.searchParams.get("id");
-  await connectMongoDB();
-  await Book.findByIdAndDelete(id);
-  return NextResponse.json({ message: "Book deleted" }, { status: 200 });
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    await connectMongoDB();
+    await Book.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Book deleted" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
