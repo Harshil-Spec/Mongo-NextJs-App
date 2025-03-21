53ef65095172681cfb8df7331fe83357088d3114
Himanshu <himanshu.mohanty@spec-india.com>
2025-03-21 13:59:03 +0530

    changes in header file

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
     //     MONGO_URI: process.env.MONGO_URI,
     //     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     //   },
+
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }, // or change to 'no-referrer'
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
