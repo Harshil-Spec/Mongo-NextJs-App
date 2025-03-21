f3a86141830717193b942fc7f1ca0302f4633228
Himanshu <himanshu.mohanty@spec-india.com>
2025-03-21 13:48:25 +0530

    Changes in code

--- components/BooksList.jsx
+++ components/BooksList.jsx
@@ -4,7 +4,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getBooks = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {
+    const res = await fetch(`/api/books`, {
       cache: "no-store",
     });
 
